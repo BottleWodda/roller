5ad456930e484866ae88b0922e99985560bdbe75
Jake Lyell <jakelyell7@gmail.com>
2022-03-17 16:15:05 +1000

    New repo for dice roller to avoid 404

---
+++ javascript.js
@@ -0,0 +1,31 @@
+
+
+function d4() {
+    let roll = Math.floor(Math.random() * 4 ) + 1;
+    document.getElementById('roll4').innerHTML = roll;
+}
+
+function d6() {
+    let roll = Math.floor(Math.random() * 6 ) + 1;
+    document.getElementById('roll6').innerHTML = roll;
+}
+
+function d8() {
+    let roll = Math.floor(Math.random() * 8 ) + 1;
+    document.getElementById('roll8').innerHTML = roll;
+}
+
+function d10() {
+    let roll = Math.floor(Math.random() * 10 ) + 1;
+    document.getElementById('roll10').innerHTML = roll;
+}
+
+function d12() {
+    let roll = Math.floor(Math.random() * 12 ) + 1;
+    document.getElementById('roll12').innerHTML = roll;
+}
+
+function d20() {
+    let roll = Math.floor(Math.random() * 20 ) + 1;
+    document.getElementById('roll20').innerHTML = roll;
+}
